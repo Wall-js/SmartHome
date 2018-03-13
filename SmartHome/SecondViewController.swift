e4199fd89bbcceb468aa7c55f0de5d8fcba62ac6
Wall <307606056@qq.com>
2018-03-13 13:06:44 +0800

    Initial Commit

---
+++ SmartHome/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SmartHome
+//
+//  Created by ckd－macpro on 2018/3/13.
+//  Copyright © 2018年 ckd－macpro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
